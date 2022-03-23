94509d3050d784da293f4a213208acb7b4b7f9f5
AlexDoss42 <alex.doss42@gmail.com>
2022-03-23 12:20:13 -0600

    adds routing to the project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import Home from "./pages/Home";
+import SearchPage from "./pages/SearchPage";
 import Header from './components/Header';
 import Footer from "./components/Footer";
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
-      <Footer />
-
-    {/* Search */}
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<SearchPage />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
